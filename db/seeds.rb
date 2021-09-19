e53a7b730b0270eddd9b3e4060b1e68fc0607f40
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-09-19 19:06:18 +0100

    Moar seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
+posts_count = 20
+comments_count = 30
+messages_count = 30
+
 users = User.create(
   [
     { name: "Alice", username: "alice", password: "password", password_confirmation: "password", email: "alice@example.com" },
@@ -10,18 +14,18 @@ users = User.create(
   ]
 )
 
-posts_attributes = Array.new(5) do
+posts_attributes = Array.new(posts_count) do
   {
     title: Faker::Company.catch_phrase,
-    body: Array.new(3){ Faker::Quote.matz }.join(" "),
+    body: Array.new(6){ Faker::Quote.matz }.join(" "),
     user: users[rand(4)] }
 end
 posts = Post.create(posts_attributes)
 
-8.times do
-  Comment.create({ body: Faker::Quote.matz, user: users[rand(4)], post: posts[rand(5)] })
+comments_count.times do
+  Comment.create({ body: Faker::Quote.matz, user: users[rand(4)], post: posts[rand(posts_count)] })
 end
 
-5.times do
+messages_count.times do
   Message.create({ body: Faker::Quote.matz, user: users[rand(4)] })
 end
